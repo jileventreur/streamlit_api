f51fcd3673d6ebc4c87e7fb90f26a4bcbcaf8f56
cadam <corentin.adam27@gmail.com>
2024-10-23 19:04:27 +0200

    update cleaning to work with resources + bug fixes

--- cleaning.py
+++ cleaning.py
@@ -7,25 +7,34 @@ from spacy.symbols import ORTH
 #ressources
 from resources.filtered_tags_set import filtered_tags_set
 import streamlit as st
+import pickle
 
-# file too big to be store in git so we have to dl it during first execution from gdrive
-stopwords_path = 'resources\stopwords.py'
-if not os.path.isfile(stopwords_path):
-    import gdown
-    url = 'https://drive.google.com/uc?id=1BMJor8iX-3ZK0-uFhJ9y3i1k6wbtgWa4'
-    gdown.download(url, stopwords_path, quiet=False)    
-from resources.stopwords import stopwords
+# # file too big to be store in git so we have to dl it during first execution from gdrive
+# stopwords_path = 'resources\stopwords.py'
+# if not os.path.isfile(stopwords_path):
+#     import gdown
+#     url = 'https://drive.google.com/uc?id=1BMJor8iX-3ZK0-uFhJ9y3i1k6wbtgWa4'
+#     gdown.download(url, stopwords_path, quiet=False)    
+# from resources.stopwords import stopwords
 
-nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+g_verbose = False
 
-# #add tags as special cases
-for tag in filtered_tags_set:
+#init global variable
+_nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+with open('resources/tags.pkl', 'rb') as file:
+  tags = pickle.load(file)
+with open('resources/vocabulary.pkl', 'rb') as file:
+  vocabulary = pickle.load(file)
+  vocabulary |= tags
+
+# #add tags as special cases in tokenizer
+for tag in tags:
   special_case = [{ORTH:tag}]
-  nlp.tokenizer.add_special_case(tag, special_case)
+  _nlp.tokenizer.add_special_case(tag, special_case)
 
 def _lemmatization(text,
                   allowed_postags=["NOUN", "ADJ", "VERB", "ADV", "PROPN"]):
-    doc = nlp(text)
+    doc = _nlp(text)
     new_text = []
     for token in doc:
         if str(token) in filtered_tags_set:
@@ -41,25 +50,27 @@ def _strip_text(txt:str) -> str:
         s.extract()
     res = soup.text
     res = res.replace('\n', ' ')
+    res = res.replace('\\n', ' ')
     res = unidecode(res)
     res = res.lower()
     return res
 
-gverbose = False
 def _verbose_write(txt: str) -> None:
-    if gverbose:
+    if g_verbose:
         st.write(txt)
 
 def clean_question(question:str, verbose=False) -> str:
-    global gverbose
-    gverbose = verbose
+    global g_verbose
+    g_verbose = verbose
+    _verbose_write('before :')
+    _verbose_write(question)
     question = _strip_text(question)
     _verbose_write('stripped question :')
     _verbose_write(question)
     question = _lemmatization(question)
     _verbose_write('lemmatized question :')
     _verbose_write(question)
-    question = ' '.join([token for token in question.split(" ") if token not in stopwords])
+    question = ' '.join([token for token in question.split(' ') if token in vocabulary])
     _verbose_write('filtered question :')
     _verbose_write(question)
     return question
